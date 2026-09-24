70414dca89c273624a57ee5eeedb87ccb6dabcdb
RitulYadav <ritulsinghyadav302@gmail.com>
2026-09-24 12:15:17 +0530

    change employee name

--- tests/addEmployee.spec.js
+++ tests/addEmployee.spec.js
@@ -2,7 +2,7 @@ import { test } from '../fixtures/pageFixtures'
 
 test('Add Empoyee',async({addEmployeePage,loggedIn})=>{
     await addEmployeePage.navigateToAddEmployee('https://opensource-demo.orangehrmlive.com/web/index.php/pim/addEmployee')
-    await addEmployeePage.addTheEmployee('Harman','deep')
+    await addEmployeePage.addTheEmployee('Harman','yadav')
     await addEmployeePage.isEmployeeAdded()
     await addEmployeePage.getGeneratedEmployeeId()
 
